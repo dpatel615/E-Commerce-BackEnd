f4a51176afc26c468e4450775fb8d43014358354
Drusti Patel <drustipatel@Drustis-MacBook-Pro.local>
2022-05-16 17:58:14 -0400

    improve code

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -14,7 +14,7 @@ Category.init(
       autoIncrement: true
     },
     // define a category_name column
-    Category_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false
     }
